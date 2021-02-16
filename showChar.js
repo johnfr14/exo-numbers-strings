9dc4cc5d13b4eb3f90c4abfca8e4f7c318d09e6d
johnfr14 <jonathan.alyra@gmail.com>
2021-02-16 15:10:16 +0100

    2nd commit

--- showChar.js
+++ showChar.js
@@ -1,4 +1,6 @@
 let str = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
 
+console.log('Livre : Les Amis de Flaubert\nAuteur : Une phrase de Salammbô de Gustave Flaubert (sois disant poto de Maupassant)\n\n\t« C’était à Mégara, faubourg de Carthage, dans les jardins d’Hamilcar ».\n\n')
+
 for(let i = 0; i < str.length; i++)
   console.log(`${str[i]} <------ index ${i} du tableau 'str'`)
