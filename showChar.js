0c6851425242eef4be34612e5b8f1e06be08fe92
johnfr14 <jonathan.alyra@gmail.com>
2021-02-16 14:50:06 +0100

    1st commit

---
+++ showChar.js
@@ -0,0 +1,4 @@
+let str = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
+
+for(let i = 0; i < str.length; i++)
+  console.log(`${str[i]} <------ index ${i} du tableau 'str'`)
